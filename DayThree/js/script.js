de6435040aea06ad2aae7ac39ebc64584be61e1a
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-10 20:17:24 -0500

    This is long overdue and opps
    Setup basic files and started coding

---
+++ DayThree/js/script.js
@@ -0,0 +1,103 @@
+/*
+Chris Fortin 1/10/2015 Day Three coding
+ */
+
+// Single Line Comment
+
+// This is an alert
+// alert("Testing to see if this connected");
+
+// Console.log - allows programmers to see information
+
+console.log("This is the console");
+
+// Declare a variable
+// We use the key word of var
+var whatever1;   // This is going to be the declaration of a variable
+
+whatever1 = 42;   // Define or definition of the variable
+console.log(whatever1);
+
+// Declare and define a variable
+var a = 2;
+console.log(a);
+
+// Simple math
+a = a+3;
+console.log(a);
+
+var b;
+b = a+3;
+console.log(b);
+console.log(a);
+
+// Find our age
+var yearOfBirth = 1996;
+
+// Age is calculated by current year minus year born
+var currentYear = 2015;
+
+var age = currentYear - yearOfBirth - 1;
+console.log(age);
+
+// Math
+// +, -, *, /
+// Find area of triangle
+// 1/2 * base * height
+var base = 7.9;
+var height = 74.3;
+
+var areaTriangle = 1/2 * base * height;
+console.log(areaTriangle);
+
+// Modulo - %
+// Gives the remainder
+
+var remainder = 32%10
+console.log(remainder);
+
+// Find if even or odd
+// Modulo by 2 %2
+// 1 if it is odd
+// 0 if even
+var evenOrOdd = 67%2
+console.log(evenOrOdd);
+
+// Assignments Operators
+/*
+= Assignment
+++ Adds 1 to the variable
+-- Subtract 1 from the variable
++=number Addition assignment
+-=number Subtraction assignment
+/=number Dividtion assignment
+*=number Multiplication assignment
+ */
+
+var counter = 1 ;
+counter++; // Counter = counter + 1;
+console.log(counter);
+
+counter--; // Counter = counter - 1;
+console.log(counter);
+
+counter +=3; // Counter = counter +3;
+console.log(counter);
+
+counter -=2; // Counter = counter -2;
+console.log(counter);
+
+counter /=2; // Counter = counter/2;
+console.log(counter);
+
+counter *=4; // counter = counter *4;
+counter.log(counter);
+
+
+
+
+
+
+
+
+
