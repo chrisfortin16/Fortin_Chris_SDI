04df4995cfb996692cec16a9f64b54e10a7aaa1c
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-10 20:52:46 -0500

    Order of Operations
    Working on oder of operation.

--- DayThree/js/script.js
+++ DayThree/js/script.js
@@ -114,6 +114,30 @@ console.log(phrase);
 var phrase2 = "I don't know! \nYou never know!";
 console.log(phrase2);
 
+// Boolean
+// Kind of light switch - either on or off
+// True or false - NOT "true" or "false"
+
+var yes = true;
+
+var nope = false;
+
+// This is not a boolean
+var wrong = "true";
+
+// Order of Operations
+// PEMDAS
+
+// Find quiz averages
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 70;
+
+// Average - add then divide how many there are
+var average = (quiz1 + quiz2 + quiz3) /3;
+console.log(average);
+
+// Do not over use parenthesis
 
 
 
