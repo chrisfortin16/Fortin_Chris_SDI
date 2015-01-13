344d5bc2178b493173e71bc2d44125af43083878
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-13 18:15:31 -0500

    working with arrays
    using numbers and calling them to the console adding names

--- DayFour/js/dayFour.js
+++ DayFour/js/dayFour.js
@@ -7,9 +7,42 @@ Day Four In Class Assignments
 
 //alert("This is an Alert test test test");
 
+// Lets create a basic array
 
+var avengersNames = ["Hulk","Thor","Captain America","Iron Man"];
 
+// Print it out to the console
+console.log(avengersNames);
 
+// Print out one specific item
+
+console.log(avengersNames[1]);
+
+// Switch a team member - Captain America for Spider Man
+avengersNames[2] = "Spider Man";
+
+console.log(avengersNames);
+
+// Add a team member
+avengersNames[4] = "Black Widow";
+
+// Print off whole roster with a new member
+console.log(avengersNames);
+
+// Use a variable as an index number
+var num = 2;
+console.log(avengersNames[num]);
+
+// Length Property
+// Dot syntax - fancy for use a period
+console.log(avengersNames.length);
+
+// Last index number is the length - 1
+
+// Recruit a new team member
+avengersNames[avengersNames.length] = "Powerman"
+
+console.log(avengersNames);
 
 
 
