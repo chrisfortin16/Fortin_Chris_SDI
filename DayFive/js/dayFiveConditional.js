2ed52a53a38bc78fa1fa84561817dbf4988ce8d5
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-15 18:55:21 -0500

    Basic If statement
    Will it run or not?

--- DayFive/js/dayFiveConditional.js
+++ DayFive/js/dayFiveConditional.js
@@ -8,9 +8,32 @@ Conditionals
 
 //alert("Testing to see if this works");
 
+ // Basic conditional statement
+
+ // Create a boolean variable to test
+ var oldEnough = false;
+
+ // If the child is old enough then print to the console you can ride
+
+ /*
+
+ if (condition to test){
+ Code to run if condition is true
+ }
+
+  */
+
+ if(oldEnough){
+  // Code inside of {} will run if the test it ture
+  // This code will be skipped if test if false
+  console.log("You can ride the coaster");
+ }
+ else{
+  console.log("You can Not ride the coaster");
+ }
+
+
  
-
-
 
 
 
