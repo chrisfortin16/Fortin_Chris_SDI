490fe1ebc9e2535503018d6eb2df7a901dd52fb3
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-15 19:18:06 -0500

    Basic If and Basic Else
    Basic If and Basic Else

--- DayFive/js/dayFiveConditional.js
+++ DayFive/js/dayFiveConditional.js
@@ -33,9 +33,23 @@ Conditionals
  }
 
 
- 
+ // Relational Expression
+
+ // If the kid is over 48" tall, then he can ride
+ var kidHeight = 50;
+
+ // Create a variable for minimum height
+ var minHeight = 48;
 
 
+ if(kidHeight > minHeight){
+  
+  console.log("You are tall enough to ride the ride");
+ } else{
+ // This code will run if the code is false
+  // There are many things I wish to do in this life time, sometimes I wish to jump out of a perfectly fine plain at 35,000 feet then play Tom Perry's Free Falling in my headphones as I float back down to mother earth.
+  console.log("You can Not ride this ride because you are not tall enough");
+ }
 
 
 
