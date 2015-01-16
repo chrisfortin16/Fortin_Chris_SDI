befdfff7f53188aefbf65ca0f04b810db441df76
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-15 19:44:27 -0500

    Using conditionals
    using if, if else and else statements

--- DayFive/js/dayFiveConditional.js
+++ DayFive/js/dayFiveConditional.js
@@ -19,6 +19,8 @@ Conditionals
 
  if (condition to test){
  Code to run if condition is true
+ } else{
+   Code to run if the condition is false
  }
 
   */
@@ -41,17 +43,62 @@ Conditionals
  // Create a variable for minimum height
  var minHeight = 48;
 
+ // Create a variable for a Shoe Lift?
+ var shoeLift = 2;
+ // Is the child tall enough with the shoe lift
 
- if(kidHeight > minHeight){
-  
+
+ if(kidHeight + shoeLift > minHeight){
+
   console.log("You are tall enough to ride the ride");
  } else{
  // This code will run if the code is false
-  // There are many things I wish to do in this life time, sometimes I wish to jump out of a perfectly fine plain at 35,000 feet then play Tom Perry's Free Falling in my headphones as I float back down to mother earth.
+  // There are many things I wish to do in this life time, sometimes I wish to jump out of a perfectly fine plain at 35,000 feet then play Tom Perry's Free Falling in my headphones as I float gracefully back down to mother earth.
   console.log("You can Not ride this ride because you are not tall enough");
  }
 
 
+
+ // What ride can the kid go on based on his height?
+
+ // If the kid is 48 and up then Space Mountain
+ // If the kid is 40 and up then Buzz Lightyear Ride
+ // Anything shooter - Whinne Da Pooh
+
+ var childHeight = 42;
+
+ if(childHeight >= 48){
+
+  console.log("You are tall enough to ride Space Mountain, Buzz Lightyear and Winnie Da Pooh");
+
+ } else if(childHeight >= 40){
+
+  console.log("You are too short to ride Space Mountain, but you can ride The Buzz Lightyear ride and Winne Da Pooh");
+
+ } else{
+  console.log("You can only ride Winne Da Pooh");
+ }
+
+/*
+
+If, else if, else statement
+
+if(condition1 to test){
+
+  Code to run if condition1 is true
+
+} else if(condition2 to test){
+
+  Condition2 will only be tested if condition1 is false
+  Code to run if condition2 is true
+
+} else{
+
+  Code to run if condition1 and condition2 are false
+
+}
+
+ */
 
 
 
