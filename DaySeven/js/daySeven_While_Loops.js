51af03add879e904f2fd557e5109887d6e4c69a0
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-20 18:00:52 -0500

    While loop and Do while loop
    Working with loops and do while loops

--- DaySeven/js/daySeven_While_Loops.js
+++ DaySeven/js/daySeven_While_Loops.js
@@ -7,8 +7,40 @@
 
 //alert("Testing to see if this works!");
 
+// Basic while loo
+// The while loop, loops though a blaock of code as long as the condition is trure
+/*
+ * var i = 0;
+ * while(condition){
+    code that runs when that condition is true.
+    Incremental change to the conuting variable -
+    OR infinite loop
+ }
+*/
+
+var counter = 0; // Initalizing counter
+while (counter < 20) {
+    // Code that will run as long as counter is less then 20
+    console.log(counter + " Somethings just never end!");
+    
+    //Change for the counter variable
+    counter++;
+}
 
 
+
+// Do While Loop
+// The code will run at least once before checking the condition. 
+
+var i = 0;
+do{
+    // This code will run before the condition is checked
+    // It will also run as long as the condition is true
+    console.log("The number is "+ i +".");
+    
+    i++
+    
+}while ( i<10 );
 
 
 
