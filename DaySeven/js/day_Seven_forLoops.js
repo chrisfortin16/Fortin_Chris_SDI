f3cd8a48554865f4edc42438eccfa1e11adb80e2
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-20 18:51:05 -0500

    working with loops and for loops
    working with loops and for loops

--- DaySeven/js/day_Seven_forLoops.js
+++ DaySeven/js/day_Seven_forLoops.js
@@ -4,4 +4,31 @@
  *SDI Campus
  *For Loops
 */
-alert("Testin to see if this works");
+//alert("Testin to see if this works");
+
+// Basic for loop structure
+/*
+for(initaliation; Condition to test; Increment of change){
+    code to run as long as the condition is ture
+}
+*/
+
+for(var i=0; i<20; i+=4){
+    // Code to run aslong as i<20
+    console.log("The value of i = "+i);
+    
+}
+
+for(var k=0; k<5; k++){
+    
+    console.log("The value of k is "+k);
+    //Add a break point
+    //If k is equal to 3 then stop the code
+    
+    
+    if (k===3) {
+        console.log("I am done");
+        break;
+    }
+    
+}
