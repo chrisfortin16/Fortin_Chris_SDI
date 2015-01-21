2d00692a560bfe290c8cb07bbd3c024207de8231
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-20 23:12:09 -0500

    completed for loops
    Learned about for loops

--- Loops_worksheet/js/ForLoop.js
+++ Loops_worksheet/js/ForLoop.js
@@ -1,1 +1,14 @@
 //alert("Testing to see if this works");
+
+var j = prompt("Please enter a value for J 1-100");
+while (isNaN(j) || j === "") {
+    //what its saying is (Is age Not a Number?) ture
+    // This code runs any time this code is not a number
+    //IF it is not a number then reprompt the user
+
+j = prompt("Only use numbers, Please enter a value for J 1-100.");
+
+}
+for(j=0; j<101; j+=1 ){
+    console.log("You could have entered "+j+" for the value of J");
+}
