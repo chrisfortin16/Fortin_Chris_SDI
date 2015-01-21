09a68b09109fd83c84a3b2679c6bc8def652d735
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-20 19:34:52 -0500

    Day seven Zombies
    set up basic files for day seven zombies

--- DaySeven/js/day_seven_beer.js
+++
@@ -1,8 +0,0 @@
-/*
- *Chris Fortin
- *1/20/15
- *SDI Campus
- *Counting Beer
- */
-
-//alert("Testing to see if this works!");
---
+++ DaySeven/js/day_seven_zombies.js
@@ -0,0 +1,36 @@
+/*
+ *Chris Fortin
+ *1/20/15
+ *SDI Campus
+ *Zombies
+ */
+//alert("Testing to see if this works");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
