d438ba04a01cab893e0891837862f69329bbf8ff
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-20 22:59:50 -0500

    Finished Do while loops
    DO while loop

--- Loops_worksheet/js/DoWhileLoop.js
+++ Loops_worksheet/js/DoWhileLoop.js
@@ -6,3 +6,36 @@
 */
 
 //alert("Testing to see if this works!");
+/*
+// Do While Loop
+// The code will run at least once before checking the condition. 
+
+var i = 0;
+do{
+    // This code will run before the condition is checked
+    // It will also run as long as the condition is true
+    console.log("The number is "+ i +".");
+    
+    i++
+    
+}while ( i<10 );
+
+//Initilizing the variable
+//Test
+
+for(var i=0; i<5; i++){
+  
+  
+    console.log("Blah");
+}
+*/
+var a = prompt("Please enter a number between 1-100");
+while (isNaN(a) || a === "") {
+  
+a = prompt("Please ONLY use numbers, Please enter your number.");
+}
+do{
+    a--
+}while( a === 0)
+
+console.log("The variable is at " +a+ " and will take " +a+ " times to get back to 0.");
