7c12f82828cc148cde794db62d141ab58151c8a1
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-22 18:26:03 -0500

    Completed looping arrays
    Completed looping arrays

--- DayEight/js/loopingArrays.js
+++ DayEight/js/loopingArrays.js
@@ -5,4 +5,47 @@
  *Looping Arrays
 */
 
-alert("What you gonna do with all that junk all that junk inside your trunk?");
+//alert("What you gonna do with all that junk all that junk inside your trunk?");
+
+
+// Basic structure of for loop over array
+// For(var i = 0; i<array.length; i++){}
+// Create an array of Scooby Doo Characters
+var names = ["Scooby", "Shaggy", "Velma", "Fred"];
+
+// Loop though the whole array and tell them they solved the case.
+
+for (var i = 0; i<names.length; i++) {
+    console.log(i);
+    
+    console.log(names[i]);
+    console.log("You solved the case "+names[i]);
+}
+
+
+// Create an array of bills and add them up
+
+var bills =[50,10,5,20,10];
+
+// create a variable to hol the total
+var totalBills = 0;
+for(var j = 0; j < bills.length; j++){
+    
+    // Add each individual to the total
+    // totalBills = totalBills + bills[j];
+    
+    // Only care about bills that are equal to or greater then $20
+    
+    if (bills[j]>=20) {
+        totalBills += bills[j];
+    }
+    
+    
+    
+    
+}
+
+console.log("The total of the bills is "+totalBills);
+
+
+
