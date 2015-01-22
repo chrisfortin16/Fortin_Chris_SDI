c9d4dd439017afbc26e7e0a2a2f278023b71ed0c
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-22 18:49:49 -0500

    basic functions
    basic functions

--- DayEight/js/Functions.js
+++ DayEight/js/Functions.js
@@ -7,6 +7,32 @@
 
 //alert("Testing to see if dwyne the rock johnson will come visit me?");
 
+// function functionName(){ code to be run }
+//function call - "go button" - functionName();
+
+// Creating a fuction that logs on to the console "Hello"
+
+function printHello() {
+    console.log("Hello! You are inside of th Print Hello Function");
+
+
+}
+
+
+// Function call to start our printHello function
+
+printHello();
+printHello();
+printHello();
+
+
+
+
+// Create a function to print more text
+function printMore() {
+    console.log("Inside of print more. Here is more text!");
+}
+printMore();
 
 
 
@@ -14,7 +40,17 @@
 
 
 
+// Create a function that calculates the area to a rectangle
 
+function calcArea() {
+    // Create variables for width, height and the area
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log("The area is "+area);
+}
+
+calcArea();
 
 
 
