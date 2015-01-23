6cab67fc882f63c289b6108d9ab693f084d608ed
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-22 19:41:00 -0500

    Day eight arguments and parameters
    Day eight arguments and parameters

---
+++ DayEight/js/day_eight_arg.js
@@ -0,0 +1,8 @@
+/*
+ *Chris Fortin
+ *SDI Campus
+ *1/22/15
+ *Arguments and Parameters
+ */
+
+//alert("Testing to see if randy wants another beer");
--- DayEight/js/day_eight_scope.js
+++
@@ -1,8 +0,0 @@
-/*
- *Chris Fortin
- *1/22/15
- *SDI Campus
- *Day Eight Scope
- */
-
-//alert("Testing to see if this stinks");
