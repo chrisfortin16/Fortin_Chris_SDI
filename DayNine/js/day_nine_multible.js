7fa7c7a8e87cb184259958f26fe112f3dcb81c7c
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-24 18:33:56 -0500

    Finished procedures starting multiple
    Finished procedures starting multiple

---
+++ DayNine/js/day_nine_multible.js
@@ -0,0 +1,29 @@
+/*
+ *Chris Fortin
+ *1/24/15
+ *SDI Campus
+ *Multible
+ */
+
+alert("Test");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- DayNine/js/day_nine_pro.js
+++ DayNine/js/day_nine_pro.js
@@ -5,3 +5,36 @@
  */
 
 //alert("Yup this her works a good!");
+
+
+
+// This is a basic or normal function
+function calcArea(width, hight) {
+    var area = width*height;
+    return area;
+}
+
+// Function call
+// Create a varibale to catch the return value!
+ var results = calcArea(4,5);
+ console.log(results);
+
+// This is a procedure
+//It does not return a value
+// List of steps to do
+function calcAreaP(width, height) {
+    
+    // Calculations
+    var area = width * height;
+    console.log(area);
+    // It will not have a return
+    
+    
+}
+
+// Function call for a procedure
+calcAreaP(6,7);
+
+alert("Test"); // Procedure
+
+var userInput = prompt("Ask a question"); // Prompting is a function
