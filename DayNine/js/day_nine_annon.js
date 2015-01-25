32e8d799c0c670fa030e842d06ee83eec7b13360
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-01-24 21:11:30 -0500

    set up basic files
    set up basic files

--- DayNine/js/day_nine_annon.js
+++ DayNine/js/day_nine_annon.js
@@ -5,7 +5,61 @@
  *Annonymas
 */
 
-alert("Testing to see if this works");
+//alert("Testing to see if this works");
+
+// Annonymous Functions
+// Widley used in JS and jQuery
+// Important in Objecty Oriented Program
+// Can be used interchagably with normal fuction, if done right
+
+// Very quick and easy
+// save on memory
+
+/*
+ * Basic structire of anon function
+ * var functionName = function(parameters){
+    Code to run goes here.
+ }
+ */
+
+// Function call BEFORE the normal function is defined
+var results2 = triArea(6,7);
+console.log("Before the function result2 is "+results2);
+
+
+/*
+ * THIS WILL NOT WORK
+ * function call needs to come after the anon function 
+ // Function call before the anon function is define
+var results4 = triAreaAnon(7,8);
+console.log("Before the anon function the restults4 function is "+results4);
+*/ 
+
+
+// Create a normal function for area of a triangle
+function triArea(b,h){
+    // Area = 1/2 * base * height
+    var area = .5 * b * h;
+    return area;
+ 
+}
+
+// Function call
+var results1 = triArea(5,6);
+console.log(results1);
+
+
+// Create an anonymous function that calc the area of a triangle
+var triAreaAnon = function(b,h){
+    var area = .5 * b * h;
+    return area;
+};
+
+// Function call after defined of anon function
+var results3 = triAreaAnon(4,5);
+
+console.log("Results of anonymous function after define is "+results3);
+
 
 
 
---
+++ Fortin_Chris_Functions_Assignmen/js/WorkingFunctions.js
@@ -0,0 +1,8 @@
+/*
+ * Chris Fortin
+ * SDI Campus
+ * 1/24/15
+ * Functions Assignment
+*/
+
+alert("Testing to see if this works");
